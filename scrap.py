088e2cff8d1f8202a1a57594fb9fc79da7a9d58b
getmykhan <mkhan7@stevens.edu>
2017-03-17 18:18:46 -0400

    Minor tweaks

--- scrap.py
+++ scrap.py
@@ -1,6 +1,6 @@
 """
-This Script scraps data for any movie from rottentomatoes
-Script Version : 2.0.6
+This Script scrapes data for any movie from rottentomatoes
+Script Version : 3.11.4
 """
 
 from bs4 import BeautifulSoup
